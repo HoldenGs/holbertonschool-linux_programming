72d849a2f73acd5c792d7f436a045ad009c14b8c
HoldenGs <holdeng96@gmail.com>
2018-08-13 06:18:06 +0000

    add definitions for all functions

--- 0x0A-malloc/malloc.h
+++ 0x0A-malloc/malloc.h
@@ -10,5 +10,13 @@
 
 void *naive_malloc(size_t size);
 
+void *_malloc(size_t size);
+
+void _free(void *ptr);
+
+void *_calloc(size_t nmemb, size_t size);
+
+void *_realloc(void *ptr, size_t size);
+
 
 #endif /* _MALLOC_H_ */
